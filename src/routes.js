38218a58c5097c429f903922f1b5a78788fbc73c
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-10-01 09:11:03 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
-const { Router } = require('express');
-const router = Router();
-
-
-router.get('/:name/:age', (req, res, next) => {
-})
-module.exports = router;
+const { Router } = require('express'); 
+ const router = Router(); 
+  
+  
+ router.get('/:name/:age', (req, res, next) => { 
+   res.status(200).send({ 
+     message: "Hello {name}, you are {age} years old" 
+   }) 
+ }) 
+ module.exports = router;
