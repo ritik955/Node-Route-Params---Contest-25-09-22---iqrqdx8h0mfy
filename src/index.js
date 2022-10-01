efcbef07ead69fd6c9c46f27d8c448adafb30e81
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-10-01 09:10:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
-app.listen(3000, () => console.log('Server running......'));
-
+const app = require('./app'); 
+ const dotenv = require('dotenv'); 
+ dotenv.config(); 
+ app.listen(3000, () => console.log('Server running......'));
